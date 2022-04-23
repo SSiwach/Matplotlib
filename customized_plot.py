18f722d426dca26ca6578a2b10a61f2f709cddae
Surender Siwach <54826291+SSiwach@users.noreply.github.com>
2022-04-24 00:06:32 +0530

    Create customized_plot.py

---
+++ customized_plot.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+x = np.linspace(0,2*np.pi,400)
+
+y = np.sin(x**2)
+
+plt.plot(x,y,'orange')
+
+plt.title("Customized Plots")
+
+#Customize grids
+
+plt.grid(True,color = "black", linewidth = "1.4",linestyle = "-.")
+
+plt.show()
